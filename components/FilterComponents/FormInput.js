5eed6b3a359456e1a15b18c9f378ee6ad7f60ba6
Kabeer-Haseja <kabeer.haseja@empglabs.com>
2022-02-21 19:09:51 +0500

    Updated Filters

---
+++ components/FilterComponents/FormInput.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Image, Text, TextInput, View,StyleSheet} from 'react-native';
+
+function FormInput(props) {
+    
+    return (
+        <View style={styles.main}>
+            <View style={styles.textView}>
+                <Image
+                    resizeMode={'contain'}
+                    tintColor={'gray'}
+                    style={styles.image}
+                    source={props.image}
+                />
+            </View>
+            
+            <View style={{width: '90%'}}>
+                <Text style={styles.textStyle}>{props.title}</Text>
+                <TextInput value={props.value} placeholder={props.placeholder} onChangeText={props.onChangeText}
+                           textAlignVertical={'top'}
+                           style={styles.textInputField}/>
+            </View>
+        </View>
+    );
+    
+}
+
+export default FormInput;
+
+const styles=StyleSheet.create({
+    main:{flexDirection: 'row', alignItems: 'center', marginTop: 20},
+    textView:{
+        height: 30,
+        width: 30,
+        marginEnd: 10,
+        borderRadius: 25,
+        backgroundColor: '#F8F9FB',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    image:{
+        height: 15,
+        width: 15,
+        tintColor: '#828DA0',
+        alignItems: 'center',
+    },
+    textStyle:{fontSize: 12, textTransform: 'capitalize', marginVertical: 10},
+    textInputField:{padding: 0, borderBottomWidth: 0.2, paddingBottom: 10}
+    
+})
